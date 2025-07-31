f9d47e7474733188a49661feba48af55b136093a
Vipin Girjapure <vipin.girjapure@gmail.com>
2025-07-31 22:39:35 +0530

    [VG] added environment variables

--- src/redux/slices/HomeSlice.tsx
+++ src/redux/slices/HomeSlice.tsx
@@ -1,44 +1,38 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { apiRequest } from "../../api/apiClient";
-
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import {apiRequest} from '../../api/apiClient';
 
 const initialState = {
-    isLoading: false,
-    homeItems: []
-}
+  isLoading: false,
+  homeItems: [],
+};
 
 export const GetHomeProducts = createAsyncThunk('GetHomeProducts', async () => {
-    const response = await apiRequest('GET', 'https://fakestoreapi.com/products');
-    if (response) {
-        return response; 
-      } else {
-        throw new Error('No products found');
-      }
-})
-
-
+  const response = await apiRequest('GET', 'https://fakestoreapi.com/products');
+  if (response) {
+    return response;
+  } else {
+    throw new Error('No products found');
+  }
+});
 
 export const HomeSlice = createSlice({
-    name: 'Home',
-    initialState: initialState,
-    reducers: {
+  name: 'Home',
+  initialState: initialState,
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(GetHomeProducts.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(GetHomeProducts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.homeItems = action.payload;
+      })
+      .addCase(GetHomeProducts.rejected, state => {
+        state.isLoading = false;
+      });
+  },
+});
 
-    },
-    extraReducers: (builder) => {
-        builder
-          .addCase(GetHomeProducts.pending, (state) => {
-            state.isLoading = true;
-          })
-          .addCase(GetHomeProducts.fulfilled, (state, action) => {
-            state.isLoading = false;
-            state.homeItems = action.payload;
-          })
-          .addCase(GetHomeProducts.rejected, (state) => {
-            state.isLoading = false;
-          });
-      },
-})
-
-
-export const { } = HomeSlice.actions
-export default HomeSlice.reducer
+export const {} = HomeSlice.actions;
+export default HomeSlice.reducer;
